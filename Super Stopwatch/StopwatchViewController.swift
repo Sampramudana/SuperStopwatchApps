b201a9b2c14ebc8317ce61be4a9f5fda419b50e9
Sampramudana <32828518+Sampramudana@users.noreply.github.com>
2017-11-21 18:26:47 +0700

    SuperStopwatchApps

---
+++ Super Stopwatch/StopwatchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StopwatchViewController.swift
+//  Super Stopwatch
+//
+//  Created by Becak Holic on 11/21/17.
+//  Copyright © 2017 Sam Pramudana. All rights reserved.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
